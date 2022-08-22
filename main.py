ae2b21c74603bd24316810418df5d44050325fbb
pascal <pascal.aubin@gmail.com>
2022-08-22 20:07:22 +0200

    1st commit

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+# programme
+import logging, datetime, locale
+from pdfrw import PdfReader, PdfWriter, PdfDict
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+locale.setlocale(locale.LC_ALL, 'fr_FR')
+
+def get_pdf_info(path):
+    pdf = PdfReader(path)
+    data = {"Periode": "Septembre 2022", "Date_af_date": "06/09/2022"}
+    print(pdf.keys())
+    print(pdf.Info)
+    print(pdf.Root.keys())
+    print('PDF has {} pages'.format(len(pdf.pages)))
+
+    for page in pdf.pages:
+        annotations = page['/Annots']
+        if annotations is None:
+            continue
+
+        for annotation in annotations:
+            if annotation['/Subtype'] == '/Widget':
+                if annotation['/T']:
+                    key = annotation['/T'].to_unicode()
+                    print(key)
+                    print(data[key])
+                    annotation.update(PdfDict(V='{}'.format(data[key])))
+    PdfWriter().write('out.pdf', pdf)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    #get_pdf_info('Quittance Loyer template.pdf')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+    dat = datetime.datetime.now()
+    print(dat.strftime('%A %d %B %Y %H:%M:%S '))
+    print(dat.month)
+    print(dat.year)
+    print(dat.strftime("%B %Y").capitalize())
+    print(dat.strftime("%x"))
